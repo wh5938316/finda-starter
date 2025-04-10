9c266869afaaad814d81357c7e951296ad2b7808
Horatio Wang <wh5938316@gmail.com>
2025-04-10 16:52:54 +0800

    chore: 添加commitlint规范提交信息

---
+++ commitlint.config.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'perf',
+        'test',
+        'chore',
+        'revert'
+      ]
+    ],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never'],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'subject-case': [0, 'never'],
+    'header-max-length': [2, 'always', 100]
+  }
+}; 
