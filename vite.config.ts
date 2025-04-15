4250ef2fff480d99e27eb9ba436e35781fd9c52f
杨伟杰 <justlikesuolong@outlook.com>
2025-04-15 17:13:36 +0800

    feat: 支持 vtjump，react 版本降级

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
 /// <reference types="vitest" />
 import react from '@vitejs/plugin-react-swc';
+import type { PluginOption } from 'vite';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import vtjump from 'vtjump';
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [
+    vtjump() as PluginOption,
     tsconfigPaths({
       parseNative: false,
     }),
